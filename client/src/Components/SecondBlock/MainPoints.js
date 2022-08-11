bdcd71c2927ad8ac2900b03f4c85a70465a59916
Jimmy Zhang <example@example.com>
2022-08-11 11:57:05 -0400

    Replaced an image URL

--- client/src/Components/SecondBlock/MainPoints.js
+++ client/src/Components/SecondBlock/MainPoints.js
@@ -35,7 +35,7 @@ function MainPoints() {
                 </div>
 
                 <div className='main-points-item item6'>
-                    <img src='https://cdn-icons.flaticon.com/png/512/3236/premium/3236424.png?token=exp=1660182263~hmac=4b77e2fb9b367f983dc497b4caa2676f' className='main-points-icon'/>
+                    <img src='https://cdn-icons.flaticon.com/png/512/2202/premium/2202999.png?token=exp=1660233681~hmac=e1eb397069b5436db198cbde7b146f12' className='main-points-icon'/>
                     <p>Our permission settings allow participants to set customized KYC and AML standards at their discretion without compromising the benefits of DeFi.</p>
                 </div>
 
